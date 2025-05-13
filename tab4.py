f71235d784ecd946fc4d97453f8126c22f125a49
Sanzhar <118342051+tssforyou@users.noreply.github.com>
2025-05-13 19:29:42 +0500

    Add files via upload

--- tab4.py
+++ tab4.py
@@ -13,14 +13,15 @@ st.set_page_config(
     layout="wide"
 )
 st.markdown("""
-<link href="https://fonts.googleapis.com/css2?family=Germania+One&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
 <style>
 html, body, [class*="css"], h1, h2, h3, h4, h5, h6,
 .stMarkdown, .stHeader, .stSubheader, .stText, .stTitle {
-    font-family: 'Germania One', cursive, sans-serif !important;
+    font-family: 'Lobster', cursive, sans-serif !important;
 }
 </style>
 """, unsafe_allow_html=True)
+
 
 
 # === THEME SWITCHER ===
